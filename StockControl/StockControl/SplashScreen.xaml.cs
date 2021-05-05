4f43d0722fbe7ee8d7cc4ebf6fa7ee517363bf89
SantaClause123 <samerhaj44@gmail.com>
2021-05-05 10:05:58 +0300

    Added an animation Start Screen
    Splash with Animation

---
+++ StockControl/StockControl/SplashScreen.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace StockControl
+{
+    /// <summary>
+    /// Interaction logic for SplashScreen.xaml
+    /// </summary>
+    public partial class SplashScreen : Window
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+            Media.Source = new Uri(Environment.CurrentDirectory + @"\LoadAnimation.gif");
+            Loading();
+        }
+        DispatcherTimer timer = new DispatcherTimer();
+
+        private void Media_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void timer_tick(object sender,EventArgs e)
+        {
+            timer.Stop();
+            LoginWindow login = new LoginWindow();
+            login.Show();
+            this.Close();
+        }
+        void Loading()
+        {
+            timer.Tick += timer_tick;
+            timer.Interval = new TimeSpan(0,0,4);
+            timer.Start();
+        }
+    }
+}
